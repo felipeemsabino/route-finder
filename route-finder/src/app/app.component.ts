7c961d7bd89211e59e6e64c87140358e82f769ad
Felipe <felipeems87@gmail.com>
2017-01-18 00:05:51 -0200

    Login page created.

--- route-finder/src/app/app.component.ts
+++ route-finder/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
+import { Login } from '../pages/login/login';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 
@@ -12,7 +13,7 @@ import { Page2 } from '../pages/page2/page2';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
+  rootPage: any = Login; // definir pagina principal
 
   pages: Array<{title: string, component: any}>;
 
--- route-finder/src/app/app.module.ts
+++ route-finder/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { Login } from '../pages/login/login';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 
 @NgModule({
   declarations: [
     MyApp,
+    Login,
     Page1,
     Page2
   ],
@@ -16,6 +18,7 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    Login,
     Page1,
     Page2
   ],
