205b88a303bab868e19b1a1817dc76af6bf7604a
Felipe <felipeems87@gmail.com>
2017-01-23 23:05:15 -0200

    Commiting navigatin between login page and the first page of app.

--- route-finder/src/pages/login/login.ts
+++ route-finder/src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NavController, MenuController } from 'ionic-angular';
+import { Page1 } from '../page1/page1';
 
 @Component({
   selector: 'page-login',
@@ -8,8 +8,29 @@ import { NavController } from 'ionic-angular';
 })
 export class Login {
 
-  constructor(public navCtrl: NavController) {
+  private menu: MenuController = null;
+  private user: any = {'email': '', 'password': ''};
 
+  constructor(public navCtrl: NavController, public menuCtrl: MenuController) {
+    this.menu = menuCtrl;
+    this.menu.swipeEnable(false);
+  }
+
+  ionViewWillLeave() {
+    this.menu.swipeEnable(true);
+  }
+
+  /*
+  * Do the login using the user email and password.
+  */
+  doLogin () {
+
+    //JSON.stringify(user)
+    // Call the auth service and do all stuff
+    console.log('login -> email = ' + this.user.email + ' password = ' + this.user.password);
+
+    // if auth is ok, then go to another page-login
+    this.navCtrl.setRoot(Page1);
   }
 
 }
