bb358bf16736234e942df8e70585edbcb19d93fb
Felipe <felipeems87@gmail.com>
2017-02-01 16:55:17 -0200

    Adding google maps support.

--- route-finder/src/pages/page1/page1.ts
+++ route-finder/src/pages/page1/page1.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
   selector: 'page-page1',
@@ -8,8 +10,30 @@ import { NavController } from 'ionic-angular';
 })
 export class Page1 {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController) {
-    
+
   }
 
+  ionViewWillEnter(){
+    this.loadMap();
+  }
+
+  loadMap(){
+    try {
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  } catch (ex) {
+    console.log(ex);
+  }
+  }
 }
